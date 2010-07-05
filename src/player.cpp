214c20b317456c9d0911673f3642dfba436ac433
Josh Heidenreich <josh.sickmate@gmail.com>
2010-07-05 03:25:42 +0000

    add: Licence message, indent rules to all files

--- src/player.cpp
+++ src/player.cpp
@@ -1,3 +1,7 @@
+// This file is part of Chaotic Rage (c) 2010 Josh Heidenreich
+//
+// kate: tab-width 4; indent-width 4; space-indent off; word-wrap off;
+
 #include <iostream>
 #include <SDL.h>
 #include "rage.h"
--- src/rage.h
+++ src/rage.h
@@ -1,3 +1,7 @@
+// This file is part of Chaotic Rage (c) 2010 Josh Heidenreich
+//
+// kate: tab-width 4; indent-width 4; space-indent off; word-wrap off;
+
 #pragma once
 
 #include <iostream>
