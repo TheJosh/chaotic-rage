0501df13cb982bedb7e6fddaf25c9a3dc5154e30
Josh Heidenreich <josh.sickmate@gmail.com>
2011-07-22 07:52:21 +0000

    Ute obj

--- data/cr/gametypes/boredem.lua
+++ data/cr/gametypes/boredem.lua
@@ -9,12 +9,6 @@ bind_playerjoin(function(slot)
 		add_player("maniac", factions.team1, slot);
 	end);
 	
-	add_timer(2000, function()
-		add_npc("zomb", factions.team2);
-		add_npc("zomb", factions.team2);
-		add_npc("zomb", factions.team2);
-	end);
-	
 end);
 
 
