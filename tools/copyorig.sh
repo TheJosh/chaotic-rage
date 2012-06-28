4c1ddc27a2af4fd2a306345c1d7c80c7a8c4cae0
Josh Heidenreich <josh.sickmate@gmail.com>
2012-06-28 21:49:04 +0930

    Copyorig tool to work in msys

--- tools/copyorig.sh
+++ tools/copyorig.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
-find orig -mindepth 2 -name "*.obj" -or -name "*.png" -not -name "*uv*" -not -name "*render*" | xargs -I {} cp {} data/cr/animmodels/
+find orig -mindepth 2 -name "*.obj" -or -name "*.png" -not -name "*uv*" -not -name "*render*" | xargs -i"{}" cp {} data/cr/animmodels/
