e80e296050c358e2602e21bc3848ce3759cede00
Josh Heidenreich <josh.sickmate@gmail.com>
2012-07-10 13:03:58 +0930

    Fixed the random jitter
    I added
        body->setSleepingThresholds(0.0, 0.0);
    To the constructor, and it's all fixed.
    
    Now I just need to learn what that actually does...

--- src/player.cpp
+++ src/player.cpp
@@ -15,7 +15,8 @@ Player::Player(UnitType *uc, GameState *st, float x, float y, float z) : Unit(uc
 	for (int i = 0; i < 16; i++) this->key[i] = 0;
 	this->mouse_angle = 0.0;
 	
-	body->setAngularFactor(btVector3(0,0,1));
+	body->setSleepingThresholds(0.0, 0.0);
+	body->setAngularFactor(0.0);
 }
 
 Player::~Player()
