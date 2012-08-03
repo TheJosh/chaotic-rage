2119b826ae0728f5ca68ec4b1ca1ffafef4b625e
Josh Heidenreich <josh.sickmate@gmail.com>
2012-08-03 21:02:06 +0930

    Initial animmodel support for datatool

---
+++ datatool/AnimModel/animmodel_item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace datatool
+{
+    public class animmodel_item : base_item 
+    {
+        private string name;
+
+
+        public animmodel_item(string name)
+        {
+            this.name = name;
+        }
+
+        override public string getName()
+        {
+            return this.name;
+        }
+
+
+        [DescriptionAttribute("The name of this item, internal use only")]
+        [Category("General")]
+        [ConfuseString("name")]
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+
+    }
+}
