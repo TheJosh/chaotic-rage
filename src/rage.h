4f80e7af6886a228cbf1602bcdd8822cf8733520
Josh Heidenreich <josh.sickmate@gmail.com>
2012-10-06 11:40:35 +0930

    It's beta 7 time

--- src/rage.h
+++ src/rage.h
@@ -14,7 +14,7 @@
 
 // Engine features to optionally compile into the game
 #ifdef RELEASE
-	#define VERSION "Beta 6"
+	#define VERSION "Beta 7"
 #else
 	#define DEBUG_OPTIONS
 	#define VERSION "Git Master"
