8fd680e3a008ca115c551d449df8bddb6e9d2553
Josh Heidenreich <josh.sickmate@gmail.com>
2012-10-18 17:26:32 +1030

    Bullet build script - organise and document

--- libs-external/build.sh
+++ libs-external/build.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-wget http://bullet.googlecode.com/files/bullet-2.79-rev2440.tgz
 
+#
+#  Bullet Physics
+#
+
+# Fetch
+wget http://bullet.googlecode.com/files/bullet-2.79-rev2440.tgz
 tar -zvxf bullet-2.79-rev2440.tgz
 
+# Prep
 mkdir bullet-build
-
 cd bullet-build
-
 cmake ../bullet-2.79 -G "Unix Makefiles" -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=ON
 
+# Compile
 make -j4
 
-echo
-echo "All built"
-echo "Installing now."
-echo 
-
+# Install
 sudo make install
-
 sudo updatedb
-
 sudo ldconfig
 
