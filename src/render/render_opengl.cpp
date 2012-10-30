9c55cece557f35ca3f150a946d6c1d4cf357687c
Josh Heidenreich <josh.sickmate@gmail.com>
2012-10-30 13:24:20 +1030

    Fix lighting angles

--- src/render/render_opengl.cpp
+++ src/render/render_opengl.cpp
@@ -974,7 +974,7 @@ void RenderOpenGL::lights()
 
 	// Lights
 	GLfloat position[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-	GLfloat spot_torch[] = { -1.0f, -1.0f, 0.0f };
+	GLfloat spot_torch[] = { -1.0f, 0.0f, -1.0f };
 	
 	for (i = 0; i < st->curr_map->lights.size(); i++) {
 		Light * l = st->curr_map->lights[i];
@@ -988,10 +988,9 @@ void RenderOpenGL::lights()
 			} else if (l->type == 2) {
 				btTransform trans;
 				this->render_player->getRigidBody()->getMotionState()->getWorldTransform(trans);
-				glTranslatef(trans.getOrigin().getX(), trans.getOrigin().getY() + 10.0f, trans.getOrigin().getZ());
 				
-				btVector3 euler;
-				glRotatef(RAD_TO_DEG(PhysicsBullet::QuaternionToYaw(trans.getRotation())) + 90.0f, 0.0f, 1.0f, 0.0f);
+				glTranslatef(trans.getOrigin().getX(), trans.getOrigin().getY() + 10.0f, trans.getOrigin().getZ());
+				glRotatef(RAD_TO_DEG(PhysicsBullet::QuaternionToYaw(trans.getRotation())) + 90.0f + 45.0f, 0.0f, 1.0f, 0.0f);
 				
 				glLightfv(GL_LIGHT0 + i, GL_SPOT_DIRECTION, spot_torch);
 				glLightf(GL_LIGHT0 + i, GL_SPOT_CUTOFF, 20);
