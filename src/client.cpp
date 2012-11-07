cec0a061f3b2f080dc5dc3be690e278289612919
Josh Heidenreich <josh.sickmate@gmail.com>
2012-11-07 22:44:42 +1030

    Initial work on background thread loading
    Uses multiple OpenGL contexts to load the data
    behind the main thread.
    
    Only for Win32 at the moment.

--- src/client.cpp
+++ src/client.cpp
@@ -4,13 +4,24 @@
 
 #include <string>
 #include <SDL.h>
+#include <SDL_syswm.h>
 #include "rage.h"
 #include "http/client_stats.h"
 #include "util/windowicon.h"
 
-
 using namespace std;
 
+
+int threadModLoader(void *indata);
+void loadMods(GameState *st);
+
+class ThreadData {
+	public:
+		GameState * st;
+		HDC hdc;
+		HGLRC hglrc;
+		bool done;
+};
 
 
 int main (int argc, char ** argv)
@@ -54,7 +65,69 @@ int main (int argc, char ** argv)
 		i->doit();
 	#endif
 	
-	// Load data
+	// This is version 1 of the async loader...
+	#ifdef _WIN32
+		SDL_SysWMinfo info;
+		SDL_VERSION(&info.version);
+		if (! SDL_GetWMInfo(&info)) {
+			exit(1);
+		}
+
+		HDC hdc = GetDC(info.window);
+		HGLRC mainContext = wglGetCurrentContext();
+		HGLRC loaderContext = wglCreateContext(hdc);
+		wglShareLists(loaderContext, mainContext);
+
+		ThreadData * td = new ThreadData();
+		td->st = st;
+		td->hdc = hdc;
+		td->hglrc = loaderContext;
+		td->done = false;
+
+		SDL_Thread * thread = SDL_CreateThread(threadModLoader, td);
+
+		while (! td->done) {
+			cout << "Loading...\n";
+			SDL_Delay(250);
+		}
+	
+		SDL_WaitThread(thread, NULL);
+	#else
+		loadMods(st);
+	#endif
+
+
+	Menu *m = new Menu(st);
+	
+	// If you have a lot of debugging work to do, uncomment this
+	// and change your settings as required.
+	//   map, gametype, character, viewmode, num players
+	//m->startGame("tanktest", "boredem", "robot", 0, 1);
+	
+	// Run the menu
+	m->doit();
+	
+	exit(0);
+}
+
+
+int threadModLoader(void *indata)
+{
+	ThreadData * td = (ThreadData*) indata;
+	wglMakeCurrent(td->hdc, td->hglrc);
+	loadMods(td->st);
+	wglMakeCurrent(nullptr, nullptr);
+	wglDeleteContext(td->hglrc);
+	td->done = true;
+	return 0;
+}
+
+/**
+* Load the mods
+**/
+void loadMods(GameState *st)
+{
+	// Load main mod
 	Mod * mod = new Mod(st, "data/cr");
 	if (! mod->load()) {
 		reportFatalError("Unable to load data module 'cr'.");
@@ -70,20 +143,7 @@ int main (int argc, char ** argv)
 		}
 		st->mm->addMod(mod);
 	}
-	
-	Menu *m = new Menu(st);
-	
-	// If you have a lot of debugging work to do, uncomment this
-	// and change your settings as required.
-	//   map, gametype, character, viewmode, num players
-	//m->startGame("tanktest", "boredem", "robot", 0, 1);
-	
-	// Run the menu
-	m->doit();
-	
-	exit(0);
 }
-
 
 
 
