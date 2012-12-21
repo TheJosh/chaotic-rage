3279389c8f34b17f9083cb9781b3a20dc5730f8e
Josh Heidenreich <josh.sickmate@gmail.com>
2012-12-21 15:11:00 +1030

    Added missing file

---
+++ src/net/net_gameinfo.h
@@ -0,0 +1,19 @@
+// This file is part of Chaotic Rage (c) 2010 Josh Heidenreich
+//
+// kate: tab-width 4; indent-width 4; space-indent off; word-wrap off;
+
+#pragma once
+#include <iostream>
+#include <list>
+#include "../rage.h"
+#include "net.h"
+
+using namespace std;
+
+
+class NetGameinfo {
+	public:
+		string map;
+};
+
+
