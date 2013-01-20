880825ca446bc7c15a829986567b7fe3dd47e31d
Josh Heidenreich <josh.sickmate@gmail.com>
2013-01-20 23:25:11 +1030

    Beta 11

--- src/rage.h
+++ src/rage.h
@@ -14,7 +14,7 @@
 
 // Engine features to optionally compile into the game
 #ifdef RELEASE
-	#define VERSION "Beta 10"
+	#define VERSION "Beta 11"
 #else
 	#define DEBUG_OPTIONS
 	#define VERSION "Git Master"
