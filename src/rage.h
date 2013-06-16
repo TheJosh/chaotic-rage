b29497bf854531c5ad31ca34901ddb241d18dd5c
Josh Heidenreich <josh.sickmate@gmail.com>
2013-06-16 22:04:21 +0930

    Beta 18

--- src/rage.h
+++ src/rage.h
@@ -15,7 +15,7 @@
 
 // Engine features to optionally compile into the game
 #ifdef RELEASE
-	#define VERSION "Beta 17"
+	#define VERSION "Beta 18"
 #else
 	#define DEBUG_OPTIONS
 	#define VERSION "Git Master"
