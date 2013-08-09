ffc6f497fc829f846f0fec2e859603927779af3e
Josh Heidenreich <josh.sickmate@gmail.com>
2013-08-09 13:40:28 +0930

    CLI option mods

--- src/util/cmdline.cpp
+++ src/util/cmdline.cpp
@@ -26,6 +26,7 @@ void CommandLineArgs::process()
 	
 	static struct option long_options[] = {
 		{"help",			0, 0, 'h'},
+		{"version",			0, 0, 'v'},
 		{"mod",				1, 0, 'm'},
 		{"mod-list",		0, 0, 'n'},
 		{"campaign",		1, 0, 'c'},
@@ -47,17 +48,20 @@ void CommandLineArgs::process()
 		switch (c) {
 			case 'h':
 				cout <<
-					"Chaotic Rage\n\n"
-					"Options:\n"
+					"Chaotic Rage " VERSION "\n"
+					
+					"\nOptions:\n"
 					" -a\t--arcade MAP,GAME,UNIT  Play an arcade game, then exit\n"
 					" -c\t--campaign NAME         Run the specified campaign, then exit\n"
-					" -h\t--help                  Show this help\n"
 					" -j\t--join SERVER           Join a network game\n"
+					"\n"
 					" -m\t--mod NAME              Load a mod at startup\n"
 					"   \t--mod-list              List all available mods, and exit\n"
+					" -h\t--help                  Show this help\n"
+					" -v\t--version               Show version, and exit\n"
 					
 					#ifdef DEBUG_OPTIONS
-					"\n"
+					"\nDebugging:\n"
 					"   \t--debug SECTION         Enable debug mode for a given section\n"
 					"   \t--debug-list            Show a list of available debug sections\n"
 					"   \t--debug-lineno          Show file and line no in debugging output\n"
@@ -67,6 +71,11 @@ void CommandLineArgs::process()
 				exit(0);
 				break;
 			
+			case 'v':
+				cout << "Chaotic Rage " VERSION "\nhttp://www.chaoticrage.com/\n";
+				exit(0);
+				break;
+				
 			case 'm':
 				this->mod = optarg;
 				break;
