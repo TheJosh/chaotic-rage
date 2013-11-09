9a77fb5027f5acb3b9c4e2e3538b60b4ff87a1e2
Josh Heidenreich <josh.sickmate@gmail.com>
2013-11-10 07:33:24 +1030

    1.3

--- src/rage.h
+++ src/rage.h
@@ -12,7 +12,7 @@
 
 // Engine features to optionally compile into the game
 #ifdef RELEASE
-	#define VERSION "1.2"
+	#define VERSION "1.3"
 #else
 	#define DEBUG_OPTIONS
 	#define VERSION "Git Master"
