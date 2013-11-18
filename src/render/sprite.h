84f8f23880438123843b4a521d97d035f531a4a3
Josh Heidenreich <josh.sickmate@gmail.com>
2013-11-18 22:00:15 +1030

    Trying to get compiles working again

---
+++ src/render/sprite.h
@@ -0,0 +1,22 @@
+// This file is part of Chaotic Rage (c) 2010 Josh Heidenreich
+//
+// kate: tab-width 4; indent-width 4; space-indent off; word-wrap off;
+
+#pragma once
+#include "../rage.h"
+#include "gl.h"
+#include <SDL.h>
+
+
+using namespace std;
+
+
+class sprite {
+	public:
+		int w;
+		int h;
+		GLuint pixels;
+		SDL_Surface *orig;		// TODO: remove dependency on this...perhaps
+};
+
+typedef sprite* SpritePtr;
