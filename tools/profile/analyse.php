b4f53ab202c5e3845fc62d48d97b99a582e5e15c
Josh Heidenreich <josh.sickmate@gmail.com>
2014-02-06 13:21:09 +1030

    Analyse script improvements

--- tools/profile/analyse.php
+++ tools/profile/analyse.php
@@ -3,32 +3,61 @@
 * Should probably be a more CLI-style language like Python or Perl, but I can smash out PHP faster :)
 **/
 
-$file = @file_get_contents($argv[1]);
-if (! $file) die('Invalid file ' . $argv[1] . "\n");
-
-$lines = explode("\n", $file);
-array_shift($lines);
-
-$data = array();
-foreach ($lines as $ln) {
-	list($event, $st, $en, $time) = explode("\t", $ln);
-	if (! $event) continue;
-	
-	if (! $data[$event]['min']) $data[$event]['min'] = 9999;
-	
-	$data[$event]['total'] += $time;
-	$data[$event]['min'] = min($data[$event]['min'], $time);
-	$data[$event]['max'] = max($data[$event]['max'], $time);
-	$data[$event]['count']++;
+array_shift($argv);
+if (count($argv) == 0) {
+	die('You must provide at least one argument');
 }
 
-foreach ($data as $event => $d) {
-	$mean = round($d['total'] / $d['count'], 4);
-	
-	echo "{$event}\n";
-	echo "   Total:  {$d['total']}ms\n";
-	echo "   Min:    {$d['min']}ms\n";
-	echo "   Max:    {$d['max']}ms\n";
-	echo "   Count:  {$d['count']}\n";
-	echo "   Mean:   {$mean}ms\n";
+foreach ($argv as $filename) {
+	process($filename);
 }
+
+
+function process($filename) {
+	$file = @file_get_contents($filename);
+	if (! $file) {
+		echo 'Cannot read file ' . $filename . "\n";
+		return;
+	}
+
+	$lines = explode("\n", $file);
+	array_shift($lines);
+
+	// Aggregate data
+	$data = array();
+	foreach ($lines as $ln) {
+		list($event, $st, $en, $time) = explode("\t", $ln);
+		if (! $event) continue;
+	
+		if (! $data[$event]['min']) $data[$event]['min'] = 9999;
+	
+		$data[$event]['total'] += $time;
+		$data[$event]['min'] = min($data[$event]['min'], $time);
+		$data[$event]['max'] = max($data[$event]['max'], $time);
+		$data[$event]['count']++;
+	}
+
+	// Headings
+	echo ' === ', basename($filename), " === \n";
+	echo str_pad('Event', 15);
+	echo str_pad('Total', 10);
+	echo str_pad('Min', 10);
+	echo str_pad('Max', 10);
+	echo str_pad('Count', 10);
+	echo str_pad('Mean', 10);
+	echo "\n";
+
+	// Per event
+	foreach ($data as $event => $d) {
+		$mean = round($d['total'] / $d['count'], 4);
+	
+		echo str_pad($event, 15);
+		echo str_pad("{$d['total']}ms", 10);
+		echo str_pad("{$d['min']}ms", 10);
+		echo str_pad("{$d['max']}ms", 10);
+		echo str_pad("{$d['count']}", 10);
+		echo str_pad("{$mean}ms", 10);
+		echo "\n";
+	}
+}
+
