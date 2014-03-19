bdcdee2eba74d05444f91c1b9648a5556d419ca8
Josh Heidenreich <josh.sickmate@gmail.com>
2014-03-20 08:55:54 +1030

    Per-faction tickboxes for unit and gametype weapons

--- src/gui/newgame_weapons.cpp
+++ src/gui/newgame_weapons.cpp
@@ -27,6 +27,7 @@ DialogNewGameWeapons::DialogNewGameWeapons(DialogNewGame *parent, GameSettings *
 	this->parent = parent;
 	this->gs = gs;
 	this->factions_list = new GametypeFactionsListModel(gt);
+	this->faction = -1;
 }
 
 DialogNewGameWeapons::~DialogNewGameWeapons()
@@ -35,26 +36,50 @@ DialogNewGameWeapons::~DialogNewGameWeapons()
 }
 
 
+#define LEFT        10
+#define WIDTH       340
+#define ROWHEIGHT   27
+
+
 /**
 * Sub-dialog from the new game dialog for mucking about with weapons
 **/
 gcn::Container * DialogNewGameWeapons::setup()
 {
 	gcn::Button* button;
-	gcn::DropDown* dropdown;
+	int y = 10;
 	
 	c = new gcn::Window("Weapon Settings");
 	c->setDimension(gcn::Rectangle(0, 0, 360, 300));
 	
-	dropdown = new gcn::DropDown(this->factions_list);
-	dropdown->setPosition(10, 10);
-	dropdown->setWidth(340);
-	c->add(dropdown);
+	dd_faction = new gcn::DropDown(this->factions_list);
+	dd_faction->setPosition(LEFT, y);
+	dd_faction->setWidth(WIDTH);
+	dd_faction->addSelectionListener(this);
+	c->add(dd_faction);
+	
+	y += ROWHEIGHT;
+	
+	chk_unit = new gcn::CheckBox("Standard unit weapons");
+	chk_unit->setPosition(LEFT, y);
+	chk_unit->setWidth(WIDTH);
+	c->add(chk_unit);
+	
+	y += ROWHEIGHT;
+	
+	chk_gametype = new gcn::CheckBox("Stadard game type weapons");
+	chk_gametype->setPosition(LEFT, y);
+	chk_gametype->setWidth(WIDTH);
+	c->add(chk_gametype);
+	
+	y += ROWHEIGHT;
 	
 	button = new gcn::Button("Save Weapon Settings");
 	button->setPosition((360 - button->getWidth()) / 2, 250);
 	button->addActionListener(this);
 	c->add(button);
+	
+	this->valueChanged(NULL);
 	
 	return c;
 }
@@ -67,4 +92,28 @@ void DialogNewGameWeapons::action(const gcn::ActionEvent& actionEvent)
 {
 	this->m->remDialog(this);
 }
+
+
+/**
+* Handle changes to the faction dropdown
+**/
+void DialogNewGameWeapons::valueChanged(const gcn::SelectionEvent& selectionEvent)
+{
+	int new_faction;
+	
+	// Save old
+	if (faction != -1) {
+		gs->factions[faction].spawn_weapons_unit = chk_unit->isSelected();
+		gs->factions[faction].spawn_weapons_gametype = chk_gametype->isSelected();
+	}
+	
+	// Load new
+	new_faction = this->dd_faction->getSelected();
+	chk_unit->setSelected(gs->factions[new_faction].spawn_weapons_unit);
+	chk_gametype->setSelected(gs->factions[new_faction].spawn_weapons_gametype);
+	
+	// Update internal variable
+	faction = new_faction;
+}
+
 
--- src/gui/newgame_weapons.h
+++ src/gui/newgame_weapons.h
@@ -15,7 +15,7 @@ class DialogNewGame;
 /**
 * Sub-dialog from the new game dialog for mucking about with weapons
 **/
-class DialogNewGameWeapons : public Dialog, public gcn::ActionListener {
+class DialogNewGameWeapons : public Dialog, public gcn::ActionListener, public gcn::SelectionListener {
 	public:
 		DialogNewGameWeapons(DialogNewGame* parent, GameSettings* gs, GameType* gt);
 		~DialogNewGameWeapons();
@@ -23,10 +23,17 @@ class DialogNewGameWeapons : public Dialog, public gcn::ActionListener {
 	protected:
 		DialogNewGame* parent;
 		GameSettings* gs;
+		
 		GametypeFactionsListModel* factions_list;
+		int faction;
+		
+		gcn::DropDown* dd_faction;
+		gcn::CheckBox* chk_unit;
+		gcn::CheckBox* chk_gametype;
 		
 	public:
 		virtual gcn::Container * setup();
 		virtual void action(const gcn::ActionEvent& actionEvent);
+		virtual void valueChanged(const gcn::SelectionEvent& selectionEvent);
 };
 
