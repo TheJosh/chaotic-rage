8c608e319c74a3a73b3cada01dc4fc6e613504fb
Josh Heidenreich <josh.sickmate@gmail.com>
2014-03-28 08:37:23 +1030

    Added missing file for Android build

---
+++ tools/android/src/com/chaoticrage/game/ChaoticRage.java
@@ -0,0 +1,8 @@
+package com.chaoticrage.game;
+
+import org.libsdl.app.SDLActivity; 
+
+/* 
+ * A sample wrapper class that just calls SDLActivity 
+ */
+public class ChaoticRage extends SDLActivity { }
