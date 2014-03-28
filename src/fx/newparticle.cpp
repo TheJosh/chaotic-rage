8d1bd5aff03d46a193adc347a242eb8dd9e31c54
Josh Heidenreich <josh.sickmate@gmail.com>
2014-03-28 13:38:39 +1030

    Explosions

--- src/fx/newparticle.cpp
+++ src/fx/newparticle.cpp
@@ -120,29 +120,59 @@ void create_particles_blood_spray(GameState * st, btVector3 * location, float da
 **/
 void create_particles_explosion(GameState * st, btVector3 & location, float damage)
 {
-	SPK::Model* model = SPK::Model::create(
+	SPK::Model* model;
+	SPK::Emitter* emitter;
+	SPK::Group* group;
+	
+	/* Yellow and red fireball */
+	model = SPK::Model::create(
 			SPK::FLAG_RED | SPK::FLAG_GREEN | SPK::FLAG_BLUE | SPK::FLAG_ALPHA,
 			SPK::FLAG_ALPHA,
 			SPK::FLAG_RED | SPK::FLAG_GREEN | SPK::FLAG_BLUE
 	);
-
-	model->setParam(SPK::PARAM_ALPHA, 1.0f, 0.0f);
+	model->setParam(SPK::PARAM_ALPHA, 0.8f, 1.0f);
 	model->setParam(SPK::PARAM_RED, 0.8f, 1.0f);
 	model->setParam(SPK::PARAM_GREEN, 0.0f, 0.5f);
 	model->setParam(SPK::PARAM_BLUE, 0.0f, 0.1f);
 	model->setLifeTime(0.2f, 0.3f);
 
 	// Emitter
-	SPK::RandomEmitter* emitter = SPK::RandomEmitter::create();
+	emitter = SPK::RandomEmitter::create();
 	emitter->setZone(SPK::Point::create(SPK::Vector3D(location.x(), location.y(), location.z())));
 	emitter->setFlow(-1);
-	emitter->setTank(1000);
+	emitter->setTank(4000);
 	emitter->setForce(20.0f, 40.0f);
 
-	SPK::Group* group = SPK::Group::create(model, 1000);
+	// Create group
+	group = SPK::Group::create(model, 4000);
 	group->addEmitter(emitter);
 	group->setGravity(gravity);
+	st->addParticleGroup(group);
 	
+	/* Dustsplosion */
+	model = SPK::Model::create(
+			SPK::FLAG_RED | SPK::FLAG_GREEN | SPK::FLAG_BLUE | SPK::FLAG_ALPHA,
+			SPK::FLAG_ALPHA,
+			SPK::FLAG_RED | SPK::FLAG_GREEN | SPK::FLAG_BLUE
+	);
+	model->setParam(SPK::PARAM_ALPHA, 0.2f, 0.0f);
+	model->setParam(SPK::PARAM_RED, 0.6f, 0.8f);
+	model->setParam(SPK::PARAM_GREEN, 0.6f, 0.8f);
+	model->setParam(SPK::PARAM_BLUE, 0.6f, 0.8f);
+	model->setLifeTime(1.2f, 8.0f);
+
+	// Emitter
+	emitter = SPK::NormalEmitter::create();
+	emitter->setZone(SPK::Sphere::create(SPK::Vector3D(location.x(), location.y(), location.z()), 2.0f));
+	emitter->setFlow(-1);
+	emitter->setTank(2000);
+	emitter->setForce(3.0f, 5.0f);
+
+	// Create group
+	group = SPK::Group::create(model, 2000);
+	group->addEmitter(emitter);
+	group->setGravity(gravity);
+	group->setFriction(1.0f);
 	st->addParticleGroup(group);
 }
 
