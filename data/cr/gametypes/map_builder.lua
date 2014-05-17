b59fb265203131a150b40387e1fb62962e94ef54
Josh Heidenreich <josh.sickmate@gmail.com>
2014-05-17 14:54:48 +0930

    Map builder game type

---
+++ data/cr/gametypes/map_builder.lua
@@ -0,0 +1,44 @@
+----
+----  Map building tool
+----
+
+map_config = ""
+
+
+-- Mouse pick handler for adding vehicles
+function pickVehicle(coord)
+	prompt_text("Vehicle type", function(type)
+		world.addVehicle(type, coord.x, coord.z)
+		map_config = map_config .. "vehicle { type = \"" .. type .. "\" x = " .. coord.x .. " y = " .. coord.z .. " }\n"
+	end)
+end
+
+-- Add a toolbar; bound to "h" key
+function createToolbar()
+	toolbar = ui.addDialogButtonBar("Toolbar", {
+		"Add vehicle",
+		"Show config",
+		"Close toolbar"
+	}, function(btn)
+		add_timer(500, function()
+			toolbar:close()
+		end)
+
+		if btn == 0 then
+			mouse_pick(pickVehicle)
+		elseif btn == 1 then
+			local dialog = ui.addDialogTextBox("Map Config")
+			dialog.text = map_config
+		end
+	end)
+end
+
+-- Add a player
+bind_playerjoin(function(slot)
+	add_player(get_selected_unittype(), factions.team1, slot)
+end)
+
+-- New game; bind M key
+bind_gamestart(function()
+	ui.basicKeyPress("m", createToolbar)
+end)
