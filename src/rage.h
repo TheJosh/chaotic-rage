2a0c4a48c2665f3e3441327bafa2db50ca664612
Josh Heidenreich <josh.sickmate@gmail.com>
2014-06-15 21:20:30 +0930

    Version 1.8

--- src/rage.h
+++ src/rage.h
@@ -18,7 +18,7 @@
 
 // Engine features to optionally compile into the game
 #ifdef RELEASE
-	#define VERSION "1.7"
+	#define VERSION "1.8"
 #else
 	#define DEBUG_OPTIONS
 	#define VERSION "Git Master"
