6780250d1da95aae194b4137aeef66a46be9b2d8
Magnus Espersson <magnus.espersson@gmail.com>
2014-06-29 19:24:38 +0200

    Android: Travis scripts: Latest SDK, Build Tools and NDK.

--- tools/travis/script.sh
+++ tools/travis/script.sh
@@ -3,15 +3,15 @@
 
 if [ "$PLATFORM" == "linux" ]; then
 	make -j2 || exit 1
-	
-	
+
+
 elif [ "$PLATFORM" == "android" ]; then
 	cd tools/android
-	android-ndk-r9b/ndk-build || exit 1
+	android-ndk-r9d/ndk-build || exit 1
 	ant debug || exit 1
 	cd ../..
-	
-	
+
+
 else
 	echo 'Unknown $PLATFORM variable'
 	exit 1
