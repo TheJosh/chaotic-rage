bc7893868d30526fd5fafba123666021acdc5e39
Magnus Espersson <magnus.espersson@gmail.com>
2014-06-29 21:43:03 +0200

    Android Travis: Downgraded SDK due to bugs in latest version. More info: https://code.google.com/p/android/issues/detail?id=72419

--- tools/travis/install.sh
+++ tools/travis/install.sh
@@ -31,7 +31,7 @@ elif [ "$PLATFORM" == "android" ]; then
 	# sudo apt-get install openjdk-7-jdk
 
 	# Download and extract SDK
-	SDK_FILE="android-sdk_r23-linux.tgz"
+	SDK_FILE="android-sdk_r22.6.2-linux.tgz"
 	if [ ! -f ${SDK_FILE} ]; then
 		wget http://dl.google.com/android/${SDK_FILE} || exit 1
 	fi
@@ -44,7 +44,7 @@ elif [ "$PLATFORM" == "android" ]; then
 
 	# Install required Android components
 	echo 'y' | android update sdk -a --filter platform-tools --no-ui --force >/dev/null || exit 1
-	echo 'y' | android update sdk -a --filter build-tools-20.0.0 --no-ui --force >/dev/null || exit 1
+	echo 'y' | android update sdk -a --filter build-tools-19.1.0 --no-ui --force >/dev/null || exit 1
 	echo 'y' | android update sdk -a --filter android-10 --no-ui --force >/dev/null || exit 1
 
 	# Download and extract NDK
