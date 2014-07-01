cee5d3c9c9efd30c50395f2c92cde9bea977a824
Josh Heidenreich <josh.sickmate@gmail.com>
2014-07-01 13:37:55 +0930

    Various fixes for Lua 5.2

--- src/lua/lua_unitinfo_lib.cpp
+++ src/lua/lua_unitinfo_lib.cpp
@@ -23,7 +23,8 @@ extern "C" {
 static UnitQueryResult * _get(lua_State *L, int i)
 {
 	if (luaL_checkudata(L, i, MYTYPE) == NULL) {
-		luaL_typerror(L, i, MYTYPE);
+		const char *msg = lua_pushfstring(L, MYTYPE " expected, got %s", luaL_typename(L, i));
+		luaL_argerror(L, i, msg);
 	}
 
 	return (UnitQueryResult*) lua_touserdata(L,i);
@@ -89,7 +90,7 @@ LUA_FUNC(tostring)
 /**
 * Metatable definition
 **/
-static const luaL_reg R[] =
+static const luaL_Reg R[] =
 {
 	{ "__index",	get },
 	{ "__tostring",	tostring },
@@ -103,7 +104,11 @@ static const luaL_reg R[] =
 void load_unitinfo_lib(lua_State *L)
 {
 	luaL_newmetatable(L,MYTYPE);
-	luaL_register(L,NULL,R);
+	#if LUA_VERSION_NUM == 502
+		luaL_setfuncs(L, R, 0);
+	#elif LUA_VERSION_NUM == 501
+		luaL_register(L,NULL,R);
+	#endif
 }
 
 /**
