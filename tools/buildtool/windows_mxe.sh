70e317f6495ad2d497e4d2518396e1ddb2dfed70
Josh Heidenreich <josh.sickmate@gmail.com>
2014-07-02 10:13:58 +0930

    Buildtool updates for MXE builds

--- tools/buildtool/windows_mxe.sh
+++ tools/buildtool/windows_mxe.sh
@@ -10,11 +10,17 @@ make MXE=~/mxe
 mkdir "$DISTTMP"
 mv chaoticrage.exe "$DISTTMP"
 cp -r data "$DISTTMP"
+cp -r maps "$DISTTMP"
+cp -r README.md "$DISTTMP"
+cp -r LICENSE "$DISTTMP"
+
+# Everything else is (currently) compiled statically
+# except Assimp for some reason. This is probably a bug in MXE.
 cp ~/mxe/usr/i686-w64-mingw32.static/lib/libassimp.dll "$DISTTMP"
 
 # Compress
-tar -cvjf "chaoticrage-mxe-$VERSION.tar.bz2" "$DISTTMP"
-mv "chaoticrage-mxe-$VERSION.tar.bz2" "$DESTDIR"
+zip -9 -r "chaoticrage-mxe-$VERSION.zip" "$DISTTMP"
+mv "chaoticrage-mxe-$VERSION.zip" "$DESTDIR"
 
 # Cleanup
 rm -r "$DISTTMP"
