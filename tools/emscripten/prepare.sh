838160699f1af8e0618f6d8d75d0b828e1fbd233
Josh Heidenreich <josh.sickmate@gmail.com>
2014-09-10 08:38:40 +0930

    Emscripten assimp

--- tools/emscripten/prepare.sh
+++ tools/emscripten/prepare.sh
@@ -10,7 +10,7 @@ mkdir -p working
 cd working
 
 ## Bullet
-if [ !-f "../lib/libLinearMath.bc" ]; then
+if [ ! -f "../lib/libLinearMath.bc" ]; then
 	rm -rf bullet
 	if [ ! -f "bullet-2.82-r2704.tgz" ]; then
 		wget http://bullet.googlecode.com/files/bullet-2.82-r2704.tgz
@@ -33,7 +33,7 @@ if [ !-f "../lib/libLinearMath.bc" ]; then
 fi
 
 ## Assimp
-if [ !-f "../lib/libassimp.bc" ]; then
+if [ ! -f "../lib/libassimp.bc" ]; then
 	rm -rf assimp
 	if [ ! -f "assimp--3.0.1270-source-only.zip" ]; then
 		wget http://chaoticrage.com/dev_libs/assimp--3.0.1270-source-only.zip
@@ -41,6 +41,7 @@ if [ !-f "../lib/libassimp.bc" ]; then
 	unzip -q assimp--3.0.1270-source-only.zip || exit 1
 	mv assimp--3.0.1270-source-only assimp || exit 1
 	cd assimp
+	patch -p1 -N --binary -i ../../assimp.patch || exit 1
 	emconfigure cmake \
 		-DBUILD_STATIC_LIB=OFF \
 		-DBUILD_ASSIMP_TOOLS=OFF \
