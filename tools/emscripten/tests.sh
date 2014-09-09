1952502dcb356b92822d92ce6bb773fb10bcfbbb
Josh Heidenreich <josh.sickmate@gmail.com>
2014-09-09 21:08:54 +0930

    More tests

--- tools/emscripten/tests.sh
+++ tools/emscripten/tests.sh
@@ -8,15 +8,14 @@ set -v
 export EMCC_DEBUG=1
 
 # Clean up previous test results
-rm -f tests/*.o tests/*.bc tests/*.js tests/*.js.map tests/*.gcc tests/*.clang
+rm -f tests/*.o tests/*.bc tests/*.js tests/*.js.map tests/*.gcc
 
 # Basic hello world
 g++ tests/hello.cpp -o tests/hello.gcc
-clang++ tests/hello.cpp -o tests/hello.clang
 em++ tests/hello.cpp -o tests/hello.js
 
 # Bullet physics
 g++ tests/bullet.cpp `pkg-config --cflags bullet` -c -o tests/bullet.o
-g++ tests/bullet.o `pkg-config --libs bullet` -o tests/bullet.native
-emcc tests/bullet.cpp `pkg-config --cflags bullet` -c -o tests/bullet.bc
-emcc tests/bullet.bc lib/libBulletDynamics.bc lib/libBulletCollision.bc lib/libLinearMath.bc -o tests/bullet.js
+g++ tests/bullet.o `pkg-config --libs bullet` -o tests/bullet.gcc
+em++ tests/bullet.cpp `pkg-config --cflags bullet` -c -o tests/bullet.bc
+em++ tests/bullet.bc lib/libBulletDynamics.bc lib/libBulletCollision.bc lib/libLinearMath.bc -o tests/bullet.js
