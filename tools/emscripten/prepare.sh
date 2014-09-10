ecd09f643e553e9ec9a24b95f4e49646097fd6e9
Josh Heidenreich <josh.sickmate@gmail.com>
2014-09-10 13:45:35 +0930

    Fix emscripten freetype

--- tools/emscripten/prepare.sh
+++ tools/emscripten/prepare.sh
@@ -11,7 +11,7 @@ mkdir -p working
 cd working
 
 ## Bullet
-if [ ! -f "../lib/libLinearMath.bc" ]; then
+if [ ! -f "../lib/libLinearMath.so" ]; then
 	rm -rf bullet
 	if [ ! -f "bullet-2.82-r2704.tgz" ]; then
 		wget http://bullet.googlecode.com/files/bullet-2.82-r2704.tgz
@@ -34,7 +34,7 @@ fi
 
 
 ## Assimp
-if [ ! -f "../lib/libassimp.bc" ]; then
+if [ ! -f "../lib/libassimp.so" ]; then
 	rm -rf assimp
 	if [ ! -f "assimp--3.0.1270-source-only.zip" ]; then
 		wget http://chaoticrage.com/dev_libs/assimp--3.0.1270-source-only.zip
@@ -58,7 +58,7 @@ fi
 
 
 ## Freetype
-if [ ! -f "../lib/libfreetype.bc" ]; then
+if [ ! -f "../lib/libfreetype.so" ]; then
 	rm -rf freetype
 	if [ ! -f "freetype-2.5.3.tar.bz2" ]; then
 		wget http://download.savannah.gnu.org/releases/freetype/freetype-2.5.3.tar.bz2
@@ -66,9 +66,16 @@ if [ ! -f "../lib/libfreetype.bc" ]; then
 	tar -xjf freetype-2.5.3.tar.bz2 || exit 1
 	mv freetype-2.5.3 freetype || exit 1
 	cd freetype
-	emconfigure ./configure --prefix=`readlink -f ../..` || exit 1
-	emmake make || exit 1
-	emmake make install || exit 1
+	echo "# XXX emptied out for emscripten" > builds/exports.mk     
+	emconfigure ./configure \
+		--with-zlib=no \
+		--with-bzip2=no \
+		--with-png=no \
+		--with-harfbuzz=no \
+		--prefix=`readlink -f ../..` \
+		|| exit 1
+	make || exit 1
+	make install || exit 1
 	cd ..
 fi
 
