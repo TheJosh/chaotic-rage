6494805d30498a8f45866bc6fffd51fe5b3d063f
Magnus Espersson <magnus.espersson@gmail.com>
2014-10-20 22:48:40 +0200

    Android: Can launch the game again after development environment update. SDK updated to latest version (23.0.3). NDK updated to latest version that is not a bin file (10B). Build-tools updated to latest (21.0.0). Run the Android update in one step instead of three. App version update to 1.10. android:targetSdkVersion updated to 19 (Android 4.4), min sdk version is still 2.3.3.

--- tools/travis/script.sh
+++ tools/travis/script.sh
@@ -9,11 +9,11 @@ elif [ "$PLATFORM" == "android" ]; then
 	cd tools/android
 
 	# Clean (for local testing)
-	android-ndk-r10/ndk-build clean || exit 1
+	android-ndk-r*/ndk-build clean || exit 1
 	ant clean || exit 1
 
 	# Build
-	android-ndk-r10/ndk-build -j2 || exit 1
+	android-ndk-r*/ndk-build -j2 || exit 1
 	ant debug || exit 1
 
 	cd ../..
