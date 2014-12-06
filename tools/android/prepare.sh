7f5a86619b54e7aae890bcf3ad2e0d7a08b15bf0
Magnus Espersson <magnus.espersson@gmail.com>
2014-12-06 16:33:44 +0100

    Android prepare script: directory change bug. Added error checks to most operations to avoid similar problems. Checkout freetype repo to a branch to avoid log about headless state.

--- tools/android/prepare.sh
+++ tools/android/prepare.sh
@@ -8,90 +8,90 @@ if [ ! -d "jni/SDL" ]; then
 		wget http://www.libsdl.org/release/SDL2-2.0.1.tar.gz
 	fi
 	tar -xzf SDL2-2.0.1.tar.gz || exit 1
-	mv SDL2-2.0.1 ../jni/SDL
+	mv SDL2-2.0.1 ../jni/SDL || exit 1
 	cd ..
 fi
 
 # SDL_image
 if [ ! -d "jni/SDL_image" ]; then
-	cd working/
+	cd working/ || exit 1
 	if [ ! -f "SDL2_image-2.0.0.tar.gz" ]; then
 		wget http://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.0.tar.gz
 	fi
 	tar -xzf SDL2_image-2.0.0.tar.gz || exit 1
-	mv SDL2_image-2.0.0 ../jni/SDL_image
+	mv SDL2_image-2.0.0 ../jni/SDL_image || exit 1
 	cd ..
 fi
 
 # SDL_mixer
 if [ ! -d "jni/SDL_mixer" ]; then
-	cd working/
+	cd working/ || exit 1
 	if [ ! -f "SDL2_mixer-2.0.0.tar.gz" ]; then
 		wget http://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.tar.gz
 	fi
 	tar -xzf SDL2_mixer-2.0.0.tar.gz || exit 1
-	mv SDL2_mixer-2.0.0 ../jni/SDL_mixer
+	mv SDL2_mixer-2.0.0 ../jni/SDL_mixer || exit 1
 	cd ..
 fi
 
 # SDL_net
 if [ ! -d "jni/SDL_net" ]; then
-	cd working/
+	cd working/ || exit 1
 	if [ ! -f "SDL2_net-2.0.0.tar.gz" ]; then
 		wget http://www.libsdl.org/projects/SDL_net/release/SDL2_net-2.0.0.tar.gz
 	fi
 	tar -xzf SDL2_net-2.0.0.tar.gz || exit 1
-	mv SDL2_net-2.0.0 ../jni/SDL_net
+	mv SDL2_net-2.0.0 ../jni/SDL_net || exit 1
 	cd ..
 fi
 
 # bullet physics
 if [ ! -d "jni/bullet" ]; then
-	cd working/
+	cd working/ || exit 1
 	if [ ! -f "bullet-2.82-r2704.tgz" ]; then
 		wget http://bullet.googlecode.com/files/bullet-2.82-r2704.tgz
 	fi
 	tar -xzf bullet-2.82-r2704.tgz || exit 1
-	mv bullet-2.82-r2704 ../jni/bullet
+	mv bullet-2.82-r2704 ../jni/bullet || exit 1
 	cd ..
 fi
 
 # freetype
 if [ ! -d "jni/freetype" ]; then
-	cd jni/
+	cd jni/ || exit 1
 	git clone https://github.com/android/platform_external_freetype.git freetype
-	cd freetype
-	git checkout 8afe960626dbf326354b836fd4d63e05b6974195
+	cd freetype || exit 1
+	git checkout -b cr_branch 8afe960626dbf326354b836fd4d63e05b6974195
 	rm -rf .git
-	cd ..
+	cd ../../
 fi
 
 # assimp
 if [ ! -d "jni/assimp" ]; then
-	cd working/
+	cd working/ || exit 1
 	if [ ! -f "assimp--3.0.1270-source-only.zip" ]; then
 		wget http://chaoticrage.com/dev_libs/assimp--3.0.1270-source-only.zip
 	fi
 	unzip -q assimp--3.0.1270-source-only.zip || exit 1
-	mv assimp--3.0.1270-source-only ../jni/assimp
+	mv assimp--3.0.1270-source-only ../jni/assimp || exit 1
 	cd ..
 fi
 
 # lua
 if [ ! -d "jni/lua" ]; then
-	cd working/
+	cd working/ || exit 1
 	if [ ! -f "lua-5.1.5.tar.gz" ]; then
 		wget http://www.lua.org/ftp/lua-5.1.5.tar.gz
 	fi
 	tar -xzf lua-5.1.5.tar.gz || exit 1
-	mv lua-5.1.5 ../jni/lua
+	mv lua-5.1.5 ../jni/lua || exit 1
 	cd ..
 fi
 
 # Copy stuff from the prep dir to the JNI dir.
-cp -r prepare_data/* jni
+cp -r prepare_data/* jni || exit 1
 rm jni/README.txt
 
 # Link up src directory
 rm -f jni/src
-ln -s ../../../src jni/src
+ln -s ../../../src jni/src || exit 1
