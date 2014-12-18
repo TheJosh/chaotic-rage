1da38e292d818a880457897978ef9ba66b81b424
Josh Heidenreich <josh.sickmate@gmail.com>
2014-12-18 13:05:03 +1030

    Released version 1.11
    That's One-Point-Eleven

--- src/rage.h
+++ src/rage.h
@@ -18,7 +18,7 @@
 
 // Engine features to optionally compile into the game
 #ifdef RELEASE
-	#define VERSION "1.10"
+	#define VERSION "1.11"
 #else
 	#define DEBUG_OPTIONS
 	#define VERSION "Git Master"
