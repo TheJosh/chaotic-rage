6037675f9d9be3a472bfa877f30341997283e0f1
Josh Heidenreich <josh.sickmate@gmail.com>
2015-05-04 21:31:28 +0930

    Removed some more files used by lua libs
    Not needed now that Lua is directly in the source code

--- tools/android/prepare_data/lua/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := lua
-LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,, $(wildcard $(LOCAL_PATH)/src/*.c))
-
-include $(BUILD_STATIC_LIBRARY)
