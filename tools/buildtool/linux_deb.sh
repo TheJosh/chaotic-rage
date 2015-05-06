eece151b2aaeee68fb0698b126eb34733d59bde2
Josh Heidenreich <josh.sickmate@gmail.com>
2015-05-07 09:07:05 +0930

    Buildtool deb fixes

--- tools/buildtool/linux_deb.sh
+++ tools/buildtool/linux_deb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-rm "chaoticrage_*_amd64.deb"
+rm -f "chaoticrage_*_amd64.deb"
 
 make clean
 make deb
 
 mv "chaoticrage_*_amd64.deb" "${DESTDIR}/chaoticrage_${VERSION}_amd64.deb"
 
-rm "chaoticrage_*_amd64.changes"
-rm "chaoticrage_*_amd64.build"
-rm "chaoticrage_*_amd64.dsc"
-rm "chaoticrage_*_amd64.tar.xz"
+rm -f "chaoticrage_*_amd64.changes"
+rm -f "chaoticrage_*_amd64.build"
+rm -f "chaoticrage_*_amd64.dsc"
+rm -f "chaoticrage_*_amd64.tar.xz"
