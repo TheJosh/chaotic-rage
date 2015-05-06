6c293dfc3ad7043428309d74967fe61282270068
Josh Heidenreich <josh.sickmate@gmail.com>
2015-05-07 09:20:13 +0930

    Trying to get buildtool deb files working

--- tools/buildtool/linux_deb.sh
+++ tools/buildtool/linux_deb.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
-rm -f "chaoticrage_*_amd64.deb"
+rm -f chaoticrage_*_amd64.deb
 
 make clean
 make deb
 
-mv "chaoticrage_*_amd64.deb" "${DESTDIR}/chaoticrage_${VERSION}_amd64.deb"
+# The deb filename will ALWAYS be the version number
+# It ignores our $VERSION string
+# Just grab whatever file was created and rename it as required
+mv chaoticrage_*_amd64.deb "${DESTDIR}/chaoticrage_${VERSION}_amd64.deb"
 
-rm -f "chaoticrage_*_amd64.changes"
-rm -f "chaoticrage_*_amd64.build"
-rm -f "chaoticrage_*_amd64.dsc"
-rm -f "chaoticrage_*_amd64.tar.xz"
+rm -f chaoticrage_*_amd64.changes
+rm -f chaoticrage_*_amd64.build
+rm -f chaoticrage_*_amd64.dsc
+rm -f chaoticrage_*_amd64.tar.xz
