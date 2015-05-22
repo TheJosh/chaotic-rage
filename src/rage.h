1097e15a934d786f3af758b6efd9218b63a62b05
Josh Heidenreich <josh.sickmate@gmail.com>
2015-05-22 12:11:20 +0930

    Bump version to 1.13

--- src/rage.h
+++ src/rage.h
@@ -18,7 +18,7 @@
 
 // Engine features to optionally compile into the game
 #ifdef RELEASE
-	#define VERSION "1.12"
+	#define VERSION "1.13"
 #else
 	#define DEBUG_OPTIONS
 	#define VERSION "Git Master"
