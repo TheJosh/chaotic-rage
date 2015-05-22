ca360a555d889c8a1e194bd99793e687519e31ef
Josh Heidenreich <josh.sickmate@gmail.com>
2015-05-22 12:10:53 +0930

    Buildtool deb mod

--- tools/buildtool/linux_deb.sh
+++ tools/buildtool/linux_deb.sh
@@ -4,6 +4,7 @@ set -e
 # Clear these to avoid when we do the mv below
 rm -f chaoticrage_*.deb
 rm -f chaoticrage_*.dsc
+rm -f chaoticrage_*.changes
 rm -f chaoticrage_*.tar.xz
 
 # Do the build
@@ -16,8 +17,9 @@ make deb
 mv chaoticrage_*_amd64.deb "${DESTDIR}/chaoticrage_${VERSION}_amd64.deb"
 mv chaoticrage-data_*_all.deb "${DESTDIR}/chaoticrage-data_${VERSION}_all.deb"
 
-# Move across the source description
+# Move across the source description and changes
 mv chaoticrage_*.dsc "${DESTDIR}/chaoticrage_${VERSION}.dsc"
+mv chaoticrage_*.changes "${DESTDIR}/chaoticrage_${VERSION}.changes"
 
 # Also move across the source archive, but not the .orig archive
 rm -f chaoticrage_*.orig.tar.xz
