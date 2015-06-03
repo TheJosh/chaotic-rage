db356bf339555cc322a5e41986020de6d6097d6e
Josh Heidenreich <josh.sickmate@gmail.com>
2015-06-03 12:50:56 +0930

    Asset rebuild tool support for partial rebulding

--- tools/assets/rebuild.sh
+++ tools/assets/rebuild.sh
@@ -14,15 +14,22 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
 
+# Take first argument as a filter on model name, if specified
+if [ "$1" != "" ]; then
+	MATCH="$1*.blend"
+else
+	MATCH="*.blend"
+fi
+
 # Find files in the source directory, and process into the destination directory
 function process {
-	for FILE in $(find $1 -name "*.blend"); do
+	for FILE in $(find $1 -name "$MATCH"); do
 		echo -n "$FILE "
-	
+
 		ERROR=$((
 			blender -b "$FILE" -P tools/assets/rebuild_blender.py -- "$2" >/dev/null
 		) 2>&1)
-	
+
 		if [ "$ERROR" == "" ]; then
 			echo " $GREEN[ OK ]$NORMAL"
 		else
